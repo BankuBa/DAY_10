9661c4c8e90ef8e67810e927e13f3d6b1ac08730
BankuBa <paaekow04@gmail.com>
2024-08-14 12:50:57 +0000

    rewrote the code

--- hex.js
+++ hex.js
@@ -3,16 +3,18 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 let button = document.querySelector(".flipper_button");
 
 let background = document.querySelector(".container");
+
 let colorName = document.querySelector(".color_name");
 
 button.addEventListener("click", () => {
   let hexValue = "#";
-
-  for (i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     let randomValue = Math.floor(Math.random() * hex.length);
-    hexValue += hex[randomValue];
+    hexValue += hex[randomValue]
+    
+
     console.log(hexValue);
-    background.style.background = hexValue;
+    background.style.background = hexValue
     colorName.innerHTML = `Color : ${hexValue}`
   }
 });
